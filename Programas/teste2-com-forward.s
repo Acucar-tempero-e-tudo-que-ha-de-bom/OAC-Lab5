e9fce8c7f3a05e6b11ffd668fda2ec5b17f291da
davipatury <davi.patury@yahoo.com.br>
2021-10-30 21:59:04 -0300

    ovo mata o giuliano

--- Programas/teste2-com-forward.s
+++ Programas/teste2-com-forward.s
@@ -6,29 +6,33 @@ Vetor: .space 1
 
 .text	
 MAIN:   jal ra, INICIALIZA
+	nop
 
-	la a0,Vetor
+	lui a0, 0x00010010
 	li a1,N
 	jal ra, SHOW2
 	nop
 
-	la a0,Vetor
+	lui a0, 0x00010010
 	li a1,N
 	jal ra,SORT
 	nop
 
-	la a0,Vetor
+	lui a0, 0x00010010
 	li a1,N
 	jal ra,SHOW2
 	nop
 
 FINAL:	jal zero, FINAL
-		nop
+	nop
 
 SWAP:	slli t1,a1,2
 	add t1,a0,t1
 	lw t0,0(t1)
 	lw t2,4(t1)
+	nop
+	nop
+	nop
 	sw t2,0(t1)
 	sw t0,4(t1)
 	ret
@@ -47,12 +51,17 @@ SORT:	addi sp,sp,-20
 for1:	bge s0,s3,exit1
 	nop
 	addi s1,s0,-1
+	nop
+	nop
 for2:	blt s1,zero,exit2
 	nop
 	slli t1,s1,2
 	add t2,s2,t1
 	lw t3,0(t2)
 	lw t4,4(t2)
+	nop
+	nop
+	nop
 	bge t4,t3,exit2
 	nop
 	mv a0,s2
@@ -71,6 +80,9 @@ exit1: 	lw s0,0(sp)	# recupera os registradores e retorna
 	lw s2,8(sp)
 	lw s3,12(sp)
 	lw ra,16(sp)
+	nop
+	nop
+	nop
 	addi sp,sp,20
 	ret
 	nop
@@ -78,12 +90,15 @@ exit1: 	lw s0,0(sp)	# recupera os registradores e retorna
 SHOW:	mv t0,a0
 	mv t1,a1
 	mv t2,zero
-
+	nop
+	nop
 loop1: 	beq t2,t1,fim1
 	nop
 	li a7,1
 	lw a0,0(t0)
 	#ecall
+	nop
+	nop
 	li a7,11
 	li a0,9
 	#ecall
@@ -105,12 +120,18 @@ fim1:	li a7,11
 SHOW2:	mv t0,a0
 	mv t1,a1
 	mv t2,zero
-
+	nop
+	nop
 loop12: beq t2,t1,fim12
 	nop
 	lw a0,0(t0)		# mostra elemento do vetor em a0
+	nop
+	nop
+	nop
 	addi t0,t0,4
 	addi t2,t2,1
+	nop
+	nop
 	mv a1,t2		# mostra a posição do elemento em a1
 	jal zero, loop12
 	nop
@@ -121,70 +142,134 @@ fim12:	ret
 
 INICIALIZA:       # Inicializa a RAM de dados com o vetor a partir do endereço 0x10010000
 #Vetor:  .word 9,2,5,1,8,2,4,3,6,7,10,2,32,54,2,12,-6,3,1,78,54,23,1,54,2,65,3,6,55,31,4,-4
-	li t0,0x10010000
+	lui t0, 0x00010010
 	li a0,9
+	nop
+	nop
 	sw a0,0(t0)
 	li a0,2
+	nop
+	nop
 	sw a0,4(t0)	
 	li a0,5
+	nop
+	nop
 	sw a0,8(t0)
 	li a0,1
+	nop
+	nop
 	sw a0,12(t0)	
 	li a0,8
+	nop
+	nop
 	sw a0,16(t0)
 	li a0,2
+	nop
+	nop
 	sw a0,20(t0)	
 	li a0,4
+	nop
+	nop
 	sw a0,24(t0)
 	li a0,3
+	nop
+	nop
 	sw a0,28(t0)	
 	li a0,6
+	nop
+	nop
 	sw a0,32(t0)
 	li a0,7
+	nop
+	nop
 	sw a0,36(t0)	
 	li a0,10
+	nop
+	nop
 	sw a0,40(t0)
 	li a0,2
+	nop
+	nop
 	sw a0,44(t0)	
 	li a0,32
+	nop
+	nop
 	sw a0,48(t0)
 	li a0,54
+	nop
+	nop
 	sw a0,52(t0)	
 	li a0,2
+	nop
+	nop
 	sw a0,56(t0)
 	li a0,12
+	nop
+	nop
 	sw a0,60(t0)	
 	li a0,-6
+	nop
+	nop
 	sw a0,64(t0)
 	li a0,3
+	nop
+	nop
 	sw a0,68(t0)	
 	li a0,1
+	nop
+	nop
 	sw a0,72(t0)
 	li a0,78
+	nop
+	nop
 	sw a0,76(t0)	
 	li a0,54
+	nop
+	nop
 	sw a0,80(t0)
 	li a0,23
+	nop
+	nop
 	sw a0,84(t0)	
 	li a0,1
+	nop
+	nop
 	sw a0,88(t0)
 	li a0,54
+	nop
+	nop
 	sw a0,92(t0)	
 	li a0,2
+	nop
+	nop
 	sw a0,96(t0)
 	li a0,65
+	nop
+	nop
 	sw a0,100(t0)	
 	li a0,3
+	nop
+	nop
 	sw a0,104(t0)
 	li a0,6
+	nop
+	nop
 	sw a0,108(t0)	
 	li a0,55
+	nop
+	nop
 	sw a0,112(t0)
 	li a0,31
+	nop
+	nop
 	sw a0,116(t0)	
 	li a0,4
+	nop
+	nop
 	sw a0,120(t0)
 	li a0,-4
+	nop
+	nop
 	sw a0,124(t0)	
 
 	jalr zero, ra, 0	# ret
